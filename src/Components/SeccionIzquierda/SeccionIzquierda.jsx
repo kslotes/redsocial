fed11ff3fb0c5280e950966736e2092cf227e8ac
kslotes <44619076+kslotes@users.noreply.github.com>
2021-04-30 19:55:38 -0300

    Limpieza total del proyecto. Incorporacion React-Bootstrap y empezamos responsive.

--- src/Components/SeccionIzquierda/SeccionIzquierda.jsx
+++ src/Components/SeccionIzquierda/SeccionIzquierda.jsx
@@ -1,16 +1,10 @@
-import styled from "styled-components";
 import Login from "../../Components/Login/Login";
-
-const Seccion = styled.div`
-    width: 50%;
-    height: 80vh;
-`;
 
 const SeccionIzquierda = () => {
     return (
-        <Seccion className="container-fluid d-flex flex-column justify-content-around">
+        <div className="d-flex flex-column justify-content-around">
             <Login />
-        </Seccion>
+        </div>
     );
 };
 
